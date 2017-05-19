41ad00551715c580c101ab20514c740e4d206895
= <vfc17@yandex.ru>
2017-05-19 19:26:30 +0300

    some further split

--- side.h
+++ side.h
@@ -4,11 +4,16 @@
 #include <vector>
 #include <memory>
 #include "card.h"
+#include "fighter.h"
+#include "modificator.h"
 
 struct Side // no need for class
 {
     public:
         std::vector<std::vector<std::shared_ptr<Card>>> rows;
+
+        unsigned get_row_strength(unsigned row);
+        unsigned calculate_fighters_strength(std::vector<std::shared_ptr<Card>>& row);
 };
 
 #endif // SIDE_H
